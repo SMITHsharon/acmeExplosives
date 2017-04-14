a1ba91ce381c51d91db90b25e76dda490040234a
sharon smith <sharon.e.smith2@gmail.com>
2017-04-13 23:02:06 -0500

    ajax, Promise, and initial writing to DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,75 @@
+$(document).ready(function(){
 
-console.log("in main.js");
+var explosives = [];
+
+// constants
+var numCategories = 2;
+var numTypes = 3;
+var numProducts;
+
+
+
+function writeDOM(){
+    var domString = "";
+
+    domString += `<h1>Fireworks</h1>`;
+    var arrayIndexStart = 2;
+    for (var i=arrayIndexStart; i<numTypes+arrayIndexStart-1; i++) {
+    	domString += `<h3>${explosives[i].name}</h3>`;
+    }
+
+    domString += `<h1>Demolition</h1>`;
+    var arrayIndexStart = 4;
+    for (var i=arrayIndexStart; i<numTypes+arrayIndexStart-1; i++) {
+    	domString += `<h3>${explosives[i].name}</h3>`;
+    }
+
+	$("#explosionsOutput").append(domString);
+}
+
+
+
+var categoriesJSON = function(){
+        return new Promise(function(resolve, reject){
+            $.ajax("./db/categories.json").done(function(data1){
+                resolve(data1.categories);
+            }).fail(function(error1){
+                reject(error1);
+            });
+        });
+    };
+
+var typesJSON = function(){
+    return new Promise(function(resolve, reject){
+        $.ajax("./db/types.json").done(function(data2){
+            resolve(data2.types);
+        }).fail(function(error2){
+            reject(error2);
+        });
+    });
+};
+
+var productsJSON = function(){
+    return new Promise(function(resolve, reject){
+        $.ajax("./db/products.json").done(function(data3){
+            resolve(data3.products);
+        }).fail(function(error3){
+            reject(error3);
+        });
+    });
+};
+
+
+Promise.all([categoriesJSON(), typesJSON(), productsJSON()])
+        .then(function(resultz){
+console.log("resultz", resultz);
+            resultz.forEach(function(ajaxCalls){
+                ajaxCalls.forEach(function(acme){
+                    explosives.push(acme);
+                })
+            })
+console.log("explosives :: ", explosives);
+            writeDOM();
+        })
+
+});
