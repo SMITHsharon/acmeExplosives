5828ff4f04b71073c66618b3abf67bf36b449e66
sharon smith <sharon.e.smith2@gmail.com>
2017-04-16 23:37:05 -0500

    rewrote processing json files; dropdown menu options writing

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,23 +2,17 @@ $(document).ready(function(){
 
 var explosives = [];
 
+var categoriesArray = [];
+var typesArray = [];
+var productsArray = [];
+
 function writeDropdownMenu(){
 
 	var domString = "";
-	var thisID = -1;
-	var dropdownDone = false;
+console.log("categoriesArray.length :: ", categoriesArray.length);
+	for (var i=0; i<categoriesArray.length; i++) {
 
-	for (var i=0; i<explosives.length; i++) {
-		
-		if (thisID > explosives[i].id) {
-			dropdownDone = true;
-			i = explosives.length;
-		}
-			
-		if (!dropdownDone) {
-			thisID++;
-			domString += `<li><a id=${explosives[i].id} href="#">${explosives[i].name}</a></li>`;
-		} // <if>
+		domString += `<li><a id=${categoriesArray[i].id} href="#">${categoriesArray[i].name}</a></li>`;
 	} // <for>
 	$(".dropdown-menu").append(domString);
 } // <writeDropdownMenu
@@ -27,15 +21,20 @@ function writeDropdownMenu(){
 
 // handler for click event on a dropdown menu
 $(".dropdown-menu").on("click", function(e){
-console.log("in click");
-console.log(e.target.id);
 	writeDOM(parseInt(e.target.id));
 });
 
 
 function writeDOM(selected){
 // console.log("selected :: ", selected);
-    var domString = "";
+
+console.log("categoriesArray :: ", categoriesArray);
+console.log("typesArray :: ", typesArray);
+console.log("productsArray :: ", productsArray);
+
+	var counter = 0;
+	domString = "";
+
     $("#explosionsOutput").empty(); // clear DOM
     
     // var selected = 0;
@@ -46,8 +45,8 @@ function writeDOM(selected){
 	};
 
 	for (var i=0; i<explosives.length; i++) {
-    	if (explosives[i].category === selected) {
-     		domString += `<h3>${explosives[i].name}</h3>`;
+
+    	
 
 // explosives.forEach(function (value) {
 // console.log("value :: ", value);
@@ -55,14 +54,41 @@ function writeDOM(selected){
 // console.log("value2 :: ", value2);
 
      		$.each( explosives, function( index, value ) {
+console.log("outer loop :: index, value :: ", index, value);
+// console.log("explosives :: ", explosives);
 				$.each( value, function ( index2, value2 ) {
-					if ((value2.name !== undefined) && (value2.type === explosives[i].id)) {
+
+
+					if ((value2.name !== undefined) 
+						&& (explosives[i].category === selected)
+						&& (value2.type === explosives[i].id)) {
+
+						
+						
+
+						domString += `<div>`;
+			     		domString += `<h3>${explosives[i].name}</h3>`;
+			     		
+
+console.log("value2 / explosives[i] / i :: ", i, value2, explosives[i]);
+console.log("value2.type / explosives[i].id / i :: ", i, value2.type, explosives[i].id);
 						domString += `<h5>${value2.name}</h5>`;
 					} // <if>
+					counter++;
+					if (counter % 3 === 0) {
+						domString += `</div>`;
+						domString += `<div class="row">`;
+						domString += `<div class="explCard col-sm-3">`;
+					}
+					
+
 				}); // <each>
 			}); // <each>
-     	};
+			domString += `</div></div>`;
+     	// };
     } // <for i>
+
+	domString += `</div>`;
 	$("#explosionsOutput").append(domString);
 } // <writeDom>
 
@@ -99,16 +125,38 @@ var productsJSON = function(){
 };
 
 
-Promise.all([categoriesJSON(), typesJSON(), productsJSON()])
-        .then(function(resultz){
-console.log("resultz", resultz);
-            resultz.forEach(function(ajaxCalls){
-                ajaxCalls.forEach(function(acme){
-                    explosives.push(acme);
-                })
-            })
-console.log("explosives :: ", explosives);
-            writeDropdownMenu();
-        })
+ // Process the JSON data files into three separate arrays
+ // categories, types, products
+categoriesJSON().then(function(categoriesData){
+    categoriesArray = categoriesData;
+    return typesJSON();
+}).then(function(typesData){
+    typesData.forEach(function(types){
+        typesArray.push(types);
+    })
+    return productsJSON();
+}).then(function(productsData){
+    productsData.forEach(function(products){
+	productsArray = products;
+    })
+
+     writeDropdownMenu();
+});
+
+
+// Rewrote the following <Promise.all> to the above ^^^ to flatten the data
+// to three separated arrays
+// 
+// Promise.all([categoriesJSON(), typesJSON(), productsJSON()])
+//         .then(function(resultz){
+// console.log("resultz", resultz);
+//             resultz.forEach(function(ajaxCalls){
+//                 ajaxCalls.forEach(function(acme){
+//                     explosives.push(acme);
+//                 })
+//             })
+// console.log("explosives :: ", explosives);
+//             writeDropdownMenu();
+//         })
 
 });
